3189096fbc2de29793b5e3d218aef719ec1edd9b
zzzRYT <jinjinstar3@gmail.com>
2025-09-18 16:59:22 +0900

    LJJ - [백준, 17836]공주님을 구해라! : bfs (2++)

---
+++ LJJ/baekjoon/1802.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : 'LJJ/baekjoon/input.txt';
+const input = fs.readFileSync(filePath, 'utf8').trim().split('\n');
